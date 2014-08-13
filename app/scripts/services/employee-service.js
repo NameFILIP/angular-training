34d6071e8d9eefe53177228c2b06d93e52d9a8c8
Filip Spiridonov <filip.spiridonov@gmail.com>
2014-08-13 13:47:00 +0300

    Fix EmployeeService's data path

--- app/scripts/services/employee-service.js
+++ app/scripts/services/employee-service.js
@@ -2,5 +2,5 @@
 
 angular.module('teamManagementApp')
   .factory('EmployeeService', ['$resource', function ($resource) {
-    return $resource('http://localhost:9000/data/staff.json');
+    return $resource('/data/staff.json');
   }]);
